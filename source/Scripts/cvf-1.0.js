6a93ca24d56d0ee3d058b59cdd73a306ad12f065
orcame <orcame@outlook.com>
2014-02-28 19:02:13 +0800

    init page template

---
+++ source/Scripts/cvf-1.0.js
@@ -0,0 +1,74 @@
+﻿(function ($, global) {
+    var cvf = window.cvf = {};
+    var animateDuration = 200;
+
+    function setWindowSize(win, width, callback) {
+        win.animate({ width: width }, animateDuration, callback);
+    }
+
+    var content = cvf.content = $('.cvf-content'),
+        panorama = cvf.panorama = $('.cvf-content-panorama');
+
+    function maximizeWindow(win) {
+        win.data('width', win.width());
+        setWindowSize(win, content.width(), function () {
+            panorama.scrollLeft(panorama.scrollLeft() + win.offset().left - panorama.offset().left)
+            //panorama.animate({ scrollLeft: win.position().left }, animateDuration);
+        });
+    }
+
+    function restoreWindow(win) {
+        var width = win.data('width');
+        win.data('width', null);
+        setWindowSize(win, width);
+    }
+
+    var windowStatus = ['cvf-minimum', 'cvf-maximize'];
+    var controlsMap = {
+        'cvf-controls-maximize': {
+            convertTo: 'cvf-controls-restore',
+            windowStatus: 'cvf-maximize',
+            handler: maximizeWindow
+        },
+        'cvf-controls-restore': {
+            convertTo: 'cvf-controls-maximize',
+            windowStatus: '',
+            handler: restoreWindow
+        },
+        'cvf-controls-close': {
+            handler: function (window) {
+                window.remove();
+            }
+        },
+        'cvf-controls-minimize': {
+            windowStatus: 'cvf-minimum',
+            handler: null
+        }
+    };
+
+    panorama.click(function (ev) {
+        var t = $(ev.target);
+        if (t.parent().hasClass('cvf-controls')) {
+            var win = t.closest('[data-trigger="controls"]');
+            if (win.length == 0) {
+                return;
+            }
+            for (n in controlsMap) {
+                var map = controlsMap[n];
+                if (t.hasClass(n)) {
+                    if (map.handler) {
+                        map.handler(win);
+                    }
+                    t.removeClass(n).addClass(map.convertTo);
+                    var len = windowStatus.length;
+                    for (var idx = 0; idx < len; idx++) {
+                        win.removeClass(windowStatus[idx]);
+                    }
+                    win.addClass(map.windowStatus);
+                    return;
+                }
+            }
+        }
+    })
+
+})(jQuery, window);
---
+++ source/Scripts/cvf-1.0.nav.js
@@ -0,0 +1,86 @@
+﻿(function ($, cvf, global) {
+    if (cvf.nav) {
+        return;
+    }
+    var nav = {};
+    cvf.nav = nav;
+
+    var items = [],
+        bar = $('.cvf-nav-bar'),
+        panel = $('.cvf-nav-panel');
+
+    var regions = {
+        top: $('.cvf-nav-top-region'),
+        middle: $('.cvf-nav-middle-region'),
+        bottom: $('.cvf-nav-bottom-region')
+    }
+
+    function openNavPanel(btn) {
+        var url = btn.attr('data-url'),
+            text = btn.attr('data-text');
+        panel.removeClass('cvf-nav-panel-zoomout')
+            .addClass('cvf-nav-panel-zoomin')
+            .find('.cvf-nav-panel-heading h1').text(text);
+        panel.find('.cvf-nav-panel-body').empty();
+        if (url) {
+            panel.addClass('cvf-loading').find('.cvf-nav-panel-body').load(url, function () {
+                panel.removeClass('cvf-loading');
+            })
+        }
+    }
+
+    function closeNavPanel() {
+        panel.removeClass('cvf-nav-panel-zoomin')
+            .removeClass('cvf-loading')
+            .addClass('cvf-nav-panel-zoomout')
+            .find('.cvf-nav-panel-body').empty();
+    }
+
+    function buttonClick() {
+        var t = $(this);
+        if (t.hasClass('active')) {
+            t.removeClass('active');
+            closeNavPanel();
+        } else {
+            bar.find('.active').removeClass('active');
+            t.addClass('active');
+            openNavPanel(t);
+        }
+    }
+
+    nav.register = function (item, region) {
+        if ($.isArray(item)) {
+            var len = item.length;
+            for (var idx = 0; idx < len; idx++) {
+                nav.register(item[idx], region);
+            }
+        } else {
+            var region = regions[region] || regions.middle;
+            items.push(item);
+            var button = $('<div class="cvf-nav-button"/>'),
+                icon = $('<span class="cvf-nav-button-icon"><i class="fa"></i></span>');
+            if (item.color) {
+                icon.addClass('color-' + item.color);
+            }
+            if (item.icon) {
+                icon.find('i').addClass(item.icon);
+            }
+            button.attr('data-url', item.url)
+                .attr('data-text', item.text)
+                .append(icon);
+            if (item.showText !== false) {
+                button.append('<div class="cvf-nav-button-text">' + item.text + '</div>');
+            }
+            region.find('ul').append($('<li/>').append(button));
+            button.click(buttonClick);
+        }
+    }
+
+    cvf.panorama.click(function () {
+        bar.find('.active').removeClass('active');
+        closeNavPanel();
+    })
+
+    bar.find('.cvf-nav-button').click(buttonClick);
+
+})(jQuery, cvf, window);
